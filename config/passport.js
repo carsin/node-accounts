ac63415efd1867b0fa30f1e8f8689ec1789a69dd
carsin <carsin@users.noreply.github.com>
2017-10-22 10:28:48 -1000

    general :update

--- config/passport.js
+++ config/passport.js
@@ -18,36 +18,30 @@ module.exports = function(passport) {
 
     // LOCAL SIGNUP ============================================================
 
-    passport.use("local-signup", new localStrategy({
+    passport.use(new localStrategy({
         usernameField : "email",
         passwordField : "password",
         passReqToCallback : true,
     },
 
-    function(req, email, password, done, username) {
+    function(req, email, password, done) {
         process.nextTick(function() {
             // Check if email is in use
             User.findOne({ "local.email": email }, function(err, user) {
                 if (err) return done(err);
                 if (user) {
+                    console.log("email exists already");
                     return done(null, false, req.flash("signupMessage", "Email already in use."));
                 } else {
-                    // Check if username is in use
-                    User.findOne({ "local.username": username }, function(err, user) {
-                        if (err) return done(err);
-                        if (user) {
-                            return done(null, false, req.flash("signupMessage", "Username has been taken."));
-                        } else {
-                            var newUser = new User();
-                            newUser.local.email = email;
-                            newUser.local.username = email;
-                            newUser.local.password = newUser.generateHash(password);
-                            newUser.save(function(err) {
-                                if (err) throw err;
-                                console.log("created user successfully");
-                                return done(null, newUser);
-                            });
-                        }
+                    var newUser = new User();
+                    newUser.local.email = email;
+                    // TODO: Fix username stuff
+                    // newUser.local.username = email;
+                    newUser.local.password = newUser.generateHash(password);
+                    newUser.save(function(err) {
+                        if (err) throw err;
+                        console.log("created user " + email + "successfully");
+                        return done(null, newUser);
                     });
                 }
             });
