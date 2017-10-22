dfcb271119ded234ef20b0f11f4180723ee854ee
carsin <carsin@users.noreply.github.com>
2017-10-22 10:36:04 -1000

    fresh start

--- config/README.md
+++
@@ -1,3 +0,0 @@
-This folder contains javascript files with secret keys refrenced in other files.
-
-These files are to be kept private as to not leak any vulnerabilities or private keys.
--- config/config.public.js
+++
@@ -1,5 +0,0 @@
-// This file is for placeholder keys/secrets that you will replace with your own, should you download this project.
-
-module.exports = {
-    expressSessionSecret: "expresSessionSecret",
-};
