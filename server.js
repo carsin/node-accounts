5a3b0f372eaf111a8501a54e6a6555a71cc9acca
carsin <carsin@users.noreply.github.com>
2017-10-29 09:44:41 -1000

    add login functionality

--- server.js
+++ server.js
@@ -33,19 +33,9 @@ mongoose.connect("mongodb://localhost/social_network", {
 
 // USER MODEL ==========================================
 var userSchema = mongoose.Schema({
-    local: {
-        email: String,
-        password: String,
-    }
+    email: String,
+    password: String,
 });
-
-userSchema.methods.hashPass = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-};
-
-userSchema.methods.validatePass = (password) => {
-    return bcrypt.compareSync(password, this.local.password)
-};
 
 var User = mongoose.model("User", userSchema);
 
@@ -70,21 +60,21 @@ passport.use("local-signup", new localStrategy({
 },
 function(req, email, password, done) {
     process.nextTick(() => {
-        User.findOne({"local.email": email}, (err, user) => {
+        User.findOne({"email": email}, (err, user) => {
             if (err) return done(err);
             if (user) {
                 return done(null, false, req.flash("signupMessage", "<div class='alert alert-danger'>Email already in use.</div>"));
             } else {
                 var newUser = new User();
 
-                newUser.local.email = email;
-                newUser.local.password = newUser.hashPass(password);
+                newUser.email = email;
+                newUser.password = bcrypt.hashSync(password, 8);
+                console.log(newUser.password);
 
                 newUser.save((err) => {
                     if (err) throw err;
 
                     return done(null, newUser, req.flash("signupMessage", "<div class='alert alert-success'>Success! Created account successfully. <a href='/login'>Log in</a> here</div>"));
-
                 });
             }
 
@@ -94,18 +84,49 @@ function(req, email, password, done) {
 
 ));
 
+// log in
+passport.use("local-login", new localStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true
+},
+
+function(req, email, password, done) {
+    User.findOne({"email": email}, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, req.flash("loginMessage", "<div class='alert alert-danger'>Username or password incorrect.</div>"));
+
+        if (!bcrypt.compareSync(password, user.password)) return done(null, false, req.flash("loginMessage", "<div class='alert alert-danger'>Username or password incorrect.</div>"));
+
+        return done(null, user, req.flash("loginMessage", "<div class='alert alert-success'>Login successful! </div>"));
+    });
+}
+
+));
+
+
 // ROUTES ==========================================
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
 app.get("/signup", (req, res) => {
-    res.render('signup.ejs', {message: req.flash('signupMessage')});
+    res.render("signup.ejs", {message: req.flash("signupMessage")});
 });
 
 app.post("/signup", passport.authenticate("local-signup", {
     successRedirect: "/signup",
     failureRedirect: "/signup",
+    failureFlash: true
+}));
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs", {message: req.flash("loginMessage")});
+});
+
+app.post("/login", passport.authenticate("local-login", {
+    successRedirect: "/login",
+    failureRedirect: "/login",
     failureFlash: true
 }));
 
